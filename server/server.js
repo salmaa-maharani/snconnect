55f3192f6dd509701675384b16ef865f73335cc1
salmaa-maharani <salmaadm26@gmail.com>
2026-04-12 14:39:20 +0800

    fix client path

--- server/server.js
+++ server/server.js
@@ -14,9 +14,9 @@ const io = socketIO(server, {
 app.use(cors());
 app.use(express.json());
 const path = require('path');
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, '../../client')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/index.html'));
 });
 
 // =====================
